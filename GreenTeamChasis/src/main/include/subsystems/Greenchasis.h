c7aa2fd829f73d337653c385daed3844ca46a516
Nicholas Faulkner <nicholasf@vandenrobotics.com>
2023-11-29 16:31:41 -0800

    added pheonix

--- GreenTeamChasis/src/main/include/subsystems/Greenchasis.h
+++ GreenTeamChasis/src/main/include/subsystems/Greenchasis.h
@@ -6,7 +6,7 @@
 
 #include <frc2/command/CommandBase.h>
 #include <frc2/command/CommandHelper.h>
-#include <coroutine>/Pheonix.h>
+#include <ctre/phoenix.h>
 #include <frc/drive/DifferentialDrive.h>
 
 /**
